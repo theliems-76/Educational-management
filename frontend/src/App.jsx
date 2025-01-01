9446e5a148620bbbee82ac432aecad51963c6b17
Giap Cao <giap.caodinh711@gmail.com>
2025-01-01 22:03:58 +0700

    add front end code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,53 +3,56 @@
 // import Login from './pages/Login';
 // import NotFound from './pages/NotFound';
 
-  // Tạm thời hardcode user student để test giao diện
-  // const user = {
-  //   role: 'student'
-  // };
-  // localStorage.setItem('user', JSON.stringify(user))
-  
-    // <>
-    //   <BrowserRouter>
-    //     <Routes>
-    //       <Route path="/login" element={<Login />} />
-    //       <Route path="/not-found" element={<NotFound />} />
-    //       <Route path="/student/*" element={<StudentDashboard />} />
+// Tạm thời hardcode user student để test giao diện
+// const user = {
+//   role: 'student'
+// };
+// localStorage.setItem('user', JSON.stringify(user))
 
-    //       {/* Chuyển hướng trang root (/) đến /student/home */}
-    //       <Route path="/" element={<Navigate to="/student/home" replace />} />
+// <>
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/login" element={<Login />} />
+//       <Route path="/not-found" element={<NotFound />} />
+//       <Route path="/student/*" element={<StudentDashboard />} />
 
-    //       {/* Không cần route "*" ở đây nữa */}
-    //       {/* <Route path="*" element={<Navigate to="/student/home" replace />} /> */}
-    //     </Routes>
-    //   </BrowserRouter>
-    // </>
-    import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-    import StudentDashboard from './pages/Student/StudentDashboard';
-    import Login from './pages/Login';
-    import NotFound from './pages/NotFound';
-    import TimeTable from "./components/TimeTable"
-    import HomeworkList from './components/UI/HomeworkList';
-    import Note from "./components/UI/StudentNote"
-    import AdminDashboard from "./pages/Admin/AdminDashboard"
-    import { ThemeProvider} from '@mui/material/styles';
-    import theme from './theme'; 
-    function App() {
-      return (
-          <ThemeProvider theme={theme}>
-              <BrowserRouter>
-                <Routes>
-                  {/* <Route path="/login" element={<Login />} /> */}
-                  <Route path="/not-found" element={<NotFound />} />
-                  <Route path="/student/*" element={<StudentDashboard />} />
-                  <Route path="/timetable" element={<TimeTable/>}/>
-                  <Route path="/homework" element={<HomeworkList/>}/>
-                  <Route path="/admin/*" element={<AdminDashboard />} />
-                  <Route path="/" element={<Navigate to="/student/home" replace />} />
-                </Routes>
-              </BrowserRouter>
-          </ThemeProvider>
-      );
-    }
-    
-    export default App;
+//       {/* Chuyển hướng trang root (/) đến /student/home */}
+//       <Route path="/" element={<Navigate to="/student/home" replace />} />
+
+//       {/* Không cần route "*" ở đây nữa */}
+//       {/* <Route path="*" element={<Navigate to="/student/home" replace />} /> */}
+//     </Routes>
+//   </BrowserRouter>
+// </>
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import TimeTable from "./components/TimeTable";
+import HomeworkList from "./components/UI/HomeworkList";
+import Note from "./components/UI/StudentNote";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import StudentDashboard from "./pages/Student/StudentDashboard";
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="/student/*" element={<StudentDashboard />} />
+          <Route path="/timetable" element={<TimeTable />} />
+          <Route path="/homework" element={<HomeworkList />} />
+          <Route path="/admin/*" element={<AdminDashboard />} />
+          <Route
+            path="/"
+            element={<Navigate to="/student/profile" replace />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
